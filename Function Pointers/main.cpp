7284bd3a659ed6fca7e65731fe381d94006ef516
Latency <latencyxxx@yahoo.com>
2014-08-20 17:20:50 -0700

    .

--- Function Pointers/main.cpp
+++ Function Pointers/main.cpp
@@ -1,3 +1,6 @@
+/*
+* Topic for discussion found here http://lnkd.in/bV6XKX6
+*/
 // Where is the library for hashmap in plain old C?
 // I am not going to reinvent the wheel.
 // FOrget it, I will just do it the old fashioned way without.
@@ -8,7 +11,7 @@
 #include <ctype.h>
 #include <stdarg.h>
 
-typedef void * fptr_t; 
+typedef void * fptr_t;
 #define FUNC(func)  \
         fptr_t func(fptr_t userdata)
 #define FUNC_PTR(func)  \
@@ -16,8 +19,10 @@ typedef void * fptr_t;
 
 // Function pointer method signatures
 typedef char* (*signature0_t)();
-typedef int   (*signature1_t)(int, int);
+typedef int(*signature1_t)(int, int);
 typedef char* (*signature2_t)(char *, char *);
+
+// Signature Prototypes
 FUNC(Type1);
 FUNC(Type2);
 FUNC(Type3);
@@ -38,25 +43,22 @@ typedef struct {
 typedef struct {
   void * address;
   char * param1,
-       * param2;
+       *param2;
 } type3_t;
 
 
 //-------------------------------------------------
 // Method Commands
 //
-char * hello_command()
-{
+char * hello_command() {
   return "Hello World!";
 }
 
-int add_command(int a, int b)
-{
+int add_command(int a, int b) {
   return a + b;
 }
 
-char* append_command(char *a, char *b)
-{
+char* append_command(char *a, char *b) {
   return strcat(a, b);
 }
 
@@ -64,18 +66,16 @@ char* append_command(char *a, char *b)
 //-------------------------------------------------
 // Jump Table
 //
-struct command
-{
+#define COMMAND(name, type, method)  { #name, type, &method }
+
+struct command {
   char *name;
   FUNC_PTR(action);
   void *address;
-};
-
-#define COMMAND(name, type)  { #name, type, & name ## _command }
-
-struct command commands[] = {
-  COMMAND(hello,  Type1),
-  COMMAND(add,    Type2),
+} commands[] = {
+  COMMAND(hello, Type1, hello_command),
+  COMMAND(hello, Type2, hello_again_command),
+  COMMAND(add, Type2),
   COMMAND(append, Type3)
 };
 
@@ -83,22 +83,19 @@ struct command commands[] = {
 //-------------------------------------------------
 // Parsers for function pointer invocation
 // XXX Private Access XXX
-FUNC(Type1)
-{
+FUNC(Type1) {
   type1_t *container = (type1_t *)userdata;
   signature0_t action = (signature0_t)container->address;
   return action();
 }
 
-FUNC(Type2)
-{
+FUNC(Type2) {
   type2_t *container = (type2_t *)userdata;
   signature1_t action = (signature1_t)container->address;
   return (void *)action(container->param1, container->param2);
 }
 
-FUNC(Type3)
-{
+FUNC(Type3) {
   type3_t *container = (type3_t *)userdata;
   signature2_t action = (signature2_t)container->address;
   return (void *)action(container->param1, container->param2);
@@ -108,9 +105,8 @@ FUNC(Type3)
 //-------------------------------------------------
 // Helper method
 // XXX Public Access XXX
-char * getline(void)
-{
-  char * line = (char *) malloc(100), *linep = line;
+char * getline(void) {
+  char * line = (char *)malloc(100), *linep = line;
   size_t lenmax = 100, len = lenmax;
   int c;
 
@@ -124,7 +120,7 @@ char * getline(void)
 
     if (--len == 0) {
       len = lenmax;
-      char * linen = (char *) realloc(linep, lenmax *= 2);
+      char * linen = (char *)realloc(linep, lenmax *= 2);
 
       if (linen == NULL) {
         free(linep);
@@ -145,8 +141,7 @@ char * getline(void)
 //-------------------------------------------------
 // Entry Point
 // XXX Public Access XXX
-int main()
-{
+int main() {
   char buf[256];
   for (size_t x = 0; x < sizeof(commands); x++)
     sprintf(buf + strlen(buf), "%s%s", !strlen(buf) ? "" : " | ", commands[x].name);
@@ -185,6 +180,7 @@ int main()
           format = "%d %d";
         } else if (commands[x].action == Type3) {
           type3_t z = {
+            commands[x].address,
             va_arg(ref, char *),
             va_arg(ref, char *)
           };
@@ -208,5 +204,3 @@ int main()
 
   return 0;
 }
-
-
