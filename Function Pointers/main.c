006d26ec2fca422ffe83354b21f8172a56825239
Latency <latencyxxx@yahoo.com>
2014-08-20 20:01:37 -0700

    .

--- Function Pointers/main.c
+++ Function Pointers/main.c
@@ -12,39 +12,41 @@
 #include <stdarg.h>
 
 typedef void * fptr_t;
-#define FUNC(func)  \
-        fptr_t func(fptr_t userdata)
+#define PARAMS(args)  \
+        fptr_t Type##args(fptr_t userdata)
 #define FUNC_PTR(func)  \
         fptr_t (*func)(fptr_t userdata)
 
 // Function pointer method signatures
 typedef char* (*signature0_t)();
-typedef int(*signature1_t)(int, int);
-typedef char* (*signature2_t)(char *, char *);
+typedef char* (*signature1_t)(char *);
+typedef int   (*signature2_t)(int, int);
+typedef char* (*signature3_t)(char *, char *);
 
-// Signature Prototypes
-FUNC(Type1);
-FUNC(Type2);
-FUNC(Type3);
+// Signature Prototypes - Use a unique/hashkey value as argument
+#define SIG_TYPE(x) Type##x
+PARAMS(0);
+PARAMS(1);
+PARAMS(2);
+PARAMS(3);
 
 //-------------------------------------------------
-// Payload Encapsulation Containers
+// Encapsulation Container
 //
-typedef struct {
-  void * address;
-} type1_t;
-
-typedef struct {
-  void * address;
-  int    param1,
-         param2;
-} type2_t;
-
-typedef struct {
-  void * address;
-  char * param1,
-       *param2;
-} type3_t;
+struct signatures {
+  void *  address;
+  union {
+    char *param1;
+  } type1;
+  union {
+    int   param1,
+          param2;
+  } type2;
+  union {
+    char *param1,
+         *param2;
+  } type3;
+};
 
 
 //-------------------------------------------------
@@ -52,6 +54,10 @@ typedef struct {
 //
 char * hello_command() {
   return "Hello World!";
+}
+
+char * hello_again_command(char *a) {
+  return "Hello Again World!";
 }
 
 int add_command(int a, int b) {
@@ -73,29 +79,29 @@ struct command {
   FUNC_PTR(action);
   void *address;
 } commands[] = {
-  COMMAND(hello, Type1, hello_command),
-  COMMAND(hello, Type2, hello_again_command),
-  COMMAND(add, Type2),
-  COMMAND(append, Type3)
+  COMMAND(hello,  SIG_TYPE(0), hello_command),
+  COMMAND(hello,  SIG_TYPE(1), hello_again_command),
+  COMMAND(add,    SIG_TYPE(2), add_command),
+  COMMAND(append, SIG_TYPE(3), append_command)
 };
 
 
 //-------------------------------------------------
 // Parsers for function pointer invocation
 // XXX Private Access XXX
-FUNC(Type1) {
+PARAMS(Type1) {
   type1_t *container = (type1_t *)userdata;
   signature0_t action = (signature0_t)container->address;
   return action();
 }
 
-FUNC(Type2) {
+PARAMS(Type2) {
   type2_t *container = (type2_t *)userdata;
   signature1_t action = (signature1_t)container->address;
   return (void *)action(container->param1, container->param2);
 }
 
-FUNC(Type3) {
+PARAMS(Type3) {
   type3_t *container = (type3_t *)userdata;
   signature2_t action = (signature2_t)container->address;
   return (void *)action(container->param1, container->param2);
@@ -159,39 +165,29 @@ int main() {
     char * arg = va_arg(ref, char *);
     // Parse input
     for (size_t x = 0; x < sizeof(commands); x++) {
-      void *userdata = NULL;
       char *format;
+      struct signatures signature;
 
       if (!strcmp(commands[x].name, arg)) {
+        signature.address = commands[x].address;
+
         // Determine type for fill.
         if (commands[x].action == Type1) {
-          type1_t z = {
-            commands[x].address
-          };
-          userdata = &z;
           format = "%s";
         } else if (commands[x].action == Type2) {
-          type2_t z = {
-            commands[x].address,
-            atoi(va_arg(ref, char *)),
-            atoi(va_arg(ref, char *))
-          };
-          userdata = &z;
+          signature.type2.param1 = atoi(va_arg(ref, char *));
+          signature.type2.param2 = atoi(va_arg(ref, char *));
           format = "%d %d";
         } else if (commands[x].action == Type3) {
-          type3_t z = {
-            commands[x].address,
-            va_arg(ref, char *),
-            va_arg(ref, char *)
-          };
-          userdata = &z;
+          signature.type2.param1 = va_arg(ref, char *);
+          signature.type2.param2 = va_arg(ref, char *);
           format = "%s %s";
         } else {
           puts("Signature type not found!");
           break;
         }
 
-        fptr_t data = (commands[x].action)(&userdata);
+        fptr_t data = (commands[x].action)(&signature);
 
         // Output the data
         puts((char *)data);
